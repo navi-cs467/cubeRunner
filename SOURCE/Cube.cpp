8dcb46072776234be08b6ff23a62fb93811960ec
bfridkis <bfridkis@gmail.com>
2019-05-15 19:24:14 -0700

    added safety check

--- SOURCE/Cube.cpp
+++ SOURCE/Cube.cpp
@@ -425,7 +425,7 @@ void Cube::drawCubeDeath(int *userInput){
 	attron(COLOR_PAIR(1));
 
 	//So animation isn't skipped if last user input is enter
-	*userInput = 0;
+	//*userInput = 0;
 
 	//Death Part 1 - small ring around token
 	for(i = (row - 1); i <= (row + 4); ++i){ //draw left/right sides
