9825ad9d8d784a3302a138f0f956624cea144264
Emily Stockenbojer <stockene@flip1.engr.oregonstate.edu>
2019-05-23 20:14:29 -0700

    adding second socket for client input

--- SOURCE/Game.cpp
+++ SOURCE/Game.cpp
@@ -607,7 +607,7 @@ int Game::playGame(char host[], char port[]) {
 				}
 
 				//receive dataPort from server, send confirmation to server
-				/*char confirm[MSG_SIZE];
+				char confirm[MSG_SIZE];
 				memset(confirm, '\0', sizeof confirm);
 				sprintf(confirm, "%d", 1);
 
@@ -624,7 +624,7 @@ int Game::playGame(char host[], char port[]) {
 				//send confirmation on new connection
 				memset(confirm, '\0', sizeof confirm);
 				sprintf(confirm, "%d", playerNum);
-				sendMessage_C(inputSocket, confirm);*/
+				sendMessage_C(inputSocket, confirm);
 
 				//set isConnected to TRUE
 				isConnected = TRUE;
