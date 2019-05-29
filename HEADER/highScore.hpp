d956224dd418e8fb61f8d19e9db926828c033be9
Emily Stockenbojer <stockene@oregonstate.edu>
2019-05-29 15:35:39 -0500

    adding high score to single player

--- HEADER/highScore.hpp
+++ HEADER/highScore.hpp
@@ -13,6 +13,6 @@
 
 int isHighScore (int score);
 void addScoreSingle(int score, char* name, int hours, int minutes, int seconds, int mode);
-void addScoreMulti(int score, char* firstName, char* secondName, int hours, int minutes, int seconds, int mode;
+void addScoreMulti(int score, char* firstName, char* secondName, int hours, int minutes, int seconds, int mode);
 
 #endif
