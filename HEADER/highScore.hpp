f1967e58a158bb3e2dd455b02ec2c7c40c5eda07
Emily Stockenbojer <stockene@oregonstate.edu>
2019-05-31 02:46:06 -0500

    displaying high scores

--- HEADER/highScore.hpp
+++ HEADER/highScore.hpp
@@ -15,6 +15,6 @@
 int isHighScore (int score);
 void addScoreSingle(int score, char* name, int hours, int minutes, int seconds, int mode);
 void addScoreMulti(int score, char* firstName, char* secondName, int hours, int minutes, int seconds, int mode);
-void displayScores(void);
+void displayScores(WINDOW **subscrnGraphic);
 
 #endif
